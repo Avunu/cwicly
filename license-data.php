20c9be1b44ca466f2dfc87027f82d04e656d640a
Kevin Shenk <kevin@avu.nu>
2025-12-24 10:44:59 -0500

    init

---
+++ license-data.php
@@ -0,0 +1,26 @@
+<?php
+
+    if (!defined('ABSPATH')) {
+        exit; // Exit if accessed directly
+    }
+
+    /**
+     * This file will be overwritten in the next update, so make changes accordingly.
+     * You can also define these values directly in your wp-config.php file.
+     */
+
+    // The plugin license key.
+    if (!defined('CC_LICENSE_KEY')) {
+        define('CC_LICENSE_KEY', '8ea718af73e73a8dc0bdd026a0f8af36');
+    }
+
+    // The theme license key.
+    if (!defined('CC_LICENSE_THEME_KEY')) {
+        define('CC_LICENSE_THEME_KEY', '');
+    }
+
+    // The email linked to the license key.
+    if (!defined('CC_LICENSE_EMAIL')) {
+        define('CC_LICENSE_EMAIL', '');
+    }
+    
